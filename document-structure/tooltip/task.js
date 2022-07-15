cd707fb306fbfdff9c216c0d414bb5e5c2794e4e
Juriy-Shcherbakov <t9296298804@gmail.com>
2022-07-15 12:57:38 +0300

    home-work tooltip done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,21 @@
+document.querySelectorAll(".has-tooltip").forEach(item => {
+    
+    const tooltip = document.createElement("div");
+    tooltip.className = "tooltip";
+    item.append(tooltip);
+
+    item.addEventListener("click", (event) => {
+ 
+        const {top, left} = item.getBoundingClientRect()    
+        
+        if (tooltip.textContent === event.currentTarget.title) { 
+            tooltip.classList.toggle("tooltip_active");
+        } else {
+            tooltip.textContent = event.currentTarget.title; 
+            tooltip.setAttribute("style", `left: ${left}px; top: ${top + 20}px;`)
+            tooltip.classList.add("tooltip_active");
+        }
+
+        event.preventDefault(); 
+    })
+});
